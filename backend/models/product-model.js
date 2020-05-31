1f686338710d1fff35d991d4fadff413d1acac97
arubtsov <arseniy.rubtsov@gmail.com>
2020-05-31 19:47:42 +0300

    add product schema

---
+++ backend/models/product-model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose')
+const { String, Decimal128, Number } = Schema.Types;
+
+const Product = new Schema(
+    {
+        name: { type: String, required: true },
+        price: { type: Decimal128, required: true },
+        quantity: { type: Number, required: true },
+        colour: { type: String, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = model('products', Product)
