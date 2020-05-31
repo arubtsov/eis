2a9e899256853e2688c5cf4627e300de94d04829
arubtsov <arseniy.rubtsov@gmail.com>
2020-05-31 19:48:25 +0300

    implement basic product controller

---
+++ backend/controllers/product-controller.js
@@ -0,0 +1,71 @@
+const Product = require('../models/product-model');
+
+const createProduct = async (req, res) => {
+    const { body } = req
+    let product = null;
+
+    try {
+        product = new Product(body)
+    }
+    catch (error) {
+        return res.status(400).json({ success: false, error: error.toString() })
+    }
+
+    try {
+        await product.save()
+
+        return res.status(201).json({ success: true, id: product._id })
+    }
+    catch (error) {
+        return res.status(500).json({ success: false, error: error.toString() })
+    }
+}
+
+const updateProduct = async (req, res) => {
+    const { body } = req
+    let product = null
+
+    if (!body)
+        return res.status(400).json({ success: false })
+
+    try {
+        product = await Product.findById(req.params.id).exec()
+    }
+    catch (error) {
+        return res.status(404).json({ success: false, error: error.toString() })
+    }
+
+    try {
+        await product.update(body).exec()
+
+        return res.status(200).json({ success: true })
+    }
+    catch (error) {
+        return res.status(500).json({ success: false, error: error.toString() })
+    }
+}
+
+const deleteProject = async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id).exec()
+
+        return res.status(204).json({ success: true })
+    }
+    catch (error) {
+        return res.status(404).json({ success: false, error: error.toString() })
+    }
+}
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({}).exec()
+
+        return res.status(200).json({ success: true, data: products })
+    }
+    catch (error) {
+        return res.status(500).json({ success: false, error: error.toString() })
+    }
+}
+
+
+module.exports = { createProduct, updateProduct, deleteProject, getProducts }
