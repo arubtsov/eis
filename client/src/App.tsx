05f7c461515e58d691226e9bd0a5b2ecbb58dd5a
arubtsov <arseniy.rubtsov@gmail.com>
2020-05-31 21:43:37 +0300

    add product list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ProductList from './product-list';
 
 function App() {
   return (
     <div className="App">
-        Hello World
+        <ProductList />
     </div>
   );
 }
---
+++ client/src/product-list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductList () {
+    return (
+        <div>
+            Empty
+        </div>
+    );
+}
+
+export default ProductList;
