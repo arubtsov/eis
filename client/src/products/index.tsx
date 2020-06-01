cb05865aa4390413cd3ddd3c1f444060cdc85893
arubtsov <arseniy.rubtsov@gmail.com>
2020-06-01 10:45:46 +0300

    add basic styles for products list

--- client/src/products/index.tsx
+++ client/src/products/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import useProducts from './use-products-hook';
 import ProductItem from './product-item';
+import styles from './products.module.css';
 
 function ProductList () {
     const { products } = useProducts();
 
     return (
-        <div>
+        <div className={styles.products}>
             {products.map(item => <ProductItem key={item._id} {...item}/>)}
         </div>
     );
--- client/src/products/product-item.tsx
+++ client/src/products/product-item.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Product } from './use-products-hook';
+import styles from './products.module.css';
 
 function ProductItem (props: Product) {
     const { name, price, quantity, colour } = props;
 
     return (
-        <div className="">
-            <h3>{name}</h3>
-            <div>{price.$numberDecimal}</div>
-            <div>{quantity}</div>
-            <div>{colour}</div>
+        <div className={styles.item}>
+            <h3 className={styles.name}>{name}</h3>
+            <div>Price: {price.$numberDecimal}</div>
+            <div>{quantity ? `Quantity: ${quantity}` : 'Out of order'}</div>
+            <div>Colour: {colour}</div>
         </div>
     )
 }
